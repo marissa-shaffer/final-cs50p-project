1cb01ec19bc20f76574007d0c4c7d50becf767b0
marissa-shaffer <marissa.shaffer1@gmail.com>
2023-08-25 09:50:55 -0500

    added contact form

--- api/project.py
+++ api/project.py
@@ -1,6 +1,19 @@
 from flask import Flask, render_template
+from flask_wtf import FlaskForm
+from wtforms import StringField, validators, PasswordField, SubmitField
+from wtforms.validators import DataRequired, Email
+from flask_bootstrap import Bootstrap
+import email_validator
 
 app = Flask(__name__)
+Bootstrap(app)
+app.secret_key = "any-string-you-want-just-keep-it-secret"
+
+class contactForm(FlaskForm):
+    name = StringField(label='Name', validators=[DataRequired()])
+    email = StringField(label='Email', validators=[DataRequired(), Email(granular_message=True)])
+    message = StringField(label='Message')
+    submit = SubmitField(label="Submit")
 
 def main():
     index()
@@ -21,9 +34,12 @@ def about():
 def projects():
     return render_template("projects.html")
 
-@app.route("/contact", methods=("POST"))
+@app.route("/contact", methods=["GET", "POST"])
 def contact():
-    return render_template("contact.html")
+    cform = contactForm()
+    if cform.validate_on_submit():
+            print(f"Name:{cform.name.data}, E-mail:{cform.email.data}, message:{cform.message.data}")
+    return render_template("contact.html", form=cform)
 
 if __name__ == "__main__":
     main()   

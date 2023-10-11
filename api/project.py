c0a2192c4315424b097553b69b4c4bac6a83facd
marissa-shaffer <marissa.shaffer1@gmail.com>
2023-10-11 13:16:56 -0500

    changed to wtforms recaptcha

--- api/project.py
+++ api/project.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request, flash
-from flask_wtf import FlaskForm
+from flask_wtf import FlaskForm, RecaptchaField, Recaptcha
 from wtforms import StringField, validators, PasswordField, SubmitField, TextAreaField
 from wtforms.validators import DataRequired, Email
 from flask_bootstrap import Bootstrap
@@ -9,21 +9,24 @@ from sendgrid import SendGridAPIClient
 from sendgrid.helpers.mail import Mail
 from google_recaptcha import ReCaptcha
 
-siteKey = os.environ.get('RECAPTCHA_SITE_KEY')
-siteSecretKey = os.environ.get('RECAPTCHA_SECRET_KEY')
+#siteKey = os.environ.get('RECAPTCHA_SITE_KEY')
+#siteSecretKey = os.environ.get('RECAPTCHA_SECRET_KEY')
 appSecretKey = os.environ.get('APP_KEY')
+
+RECAPTCHA_PUBLIC_KEY = os.environ.get('RECAPTCHA_SITE_KEY')
+RECAPTCHA_PRIVATE_KEY = os.environ.get('RECAPTCHA_SECRET_KEY')
 
 app = Flask(__name__)
 Bootstrap(app)
 app.secret_key = appSecretKey
-recaptcha = ReCaptcha(app, version=2, site_key=siteKey, site_secret=siteSecretKey)
+#recaptcha = ReCaptcha(app, version=2, site_key=siteKey, site_secret=siteSecretKey)
 
 class ContactForm(FlaskForm):
     name = StringField(label='Name', validators=[DataRequired(message="Please enter your name.")])
     email = StringField(label='Email', validators=[DataRequired(message="Please enter your email address."), Email(granular_message=True)])
     subject = StringField(label='Subject', validators=[DataRequired(message="Please enter a subject.")])
     message = TextAreaField(label='Message', validators=[DataRequired(message="Please enter a Message.")])
-    recaptcha = recaptcha
+    recaptcha = RecaptchaField()
     submit = SubmitField(label="Submit")
 
 def main():
@@ -84,6 +87,7 @@ def contact():
         elif cform.recaptcha.verify() == False:
             flash('Recaptcha has Failed, try again.')
             print('Recaptcha has Failed')
+            return render_template("contact.html", form=cform)
     elif request.method == 'GET':
         return render_template("contact.html", form=cform)
     
